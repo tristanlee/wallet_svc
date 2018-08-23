dae6ed1fbd1832d5e8f36d60284b1053dcd3c8f4
tristan <tristan.lee@qq.com>
2018-08-23 22:33:25 +0800

    add balance api add ETH token contract api

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ var wallet_svc = require("./wallet_svc");
 var handle = {};
 handle["/btc/addr"] = wallet_svc.btc_addr;
 handle["/eth/addr"] = wallet_svc.eth_addr;
+handle["/btc/balance"] = wallet_svc.btc_balance;
+handle["/eth/balance"] = wallet_svc.eth_balance;
+
+handle["/cpcs/addr"] = wallet_svc.eth_addr;
+handle["/cpcs/balance"] = wallet_svc.cpcs_balance;
+
 
 server.start(router.route, handle);
--- wallet_svc.js
+++ wallet_svc.js
@@ -4,8 +4,19 @@ var http = require('http');
 var url = require('url');
 var bitcore = require('bitcore-lib');
 var eth_hdkey = require('ethereumjs-wallet/hdkey');
-//var explorers = require('bitcore-explorers');
-//var insight = new explorers.Insight();
+var Web3 = require('web3');
+var explorers = require('bitcore-explorers');
+var insight = new explorers.Insight();
+const {promisify} = require('util');
+
+
+var provider = new Web3.providers.HttpProvider("https://mainnet.infura.io/MEDIUMTUTORIAL");
+//var provider = new Web3.providers.HttpProvider("https://ropsten.infura.io/MEDIUMTUTORIAL");
+//var provider = new Web3.providers.HttpProvider("http://192.168.1.102:8545");
+var web3 = new Web3(provider);
+var utils = web3.utils;
+var eth = web3.eth;
+
 
 const btc_xpub = 'xpub6EetK7UxfgjeepqWEkSK55JpQG4nesR77wbECVHfmHrMc19Zw3FDWAr1XpJ7xHxLQBwrgsik2qY98hXRSbR2DsSyCCRtreJduHzfmpSj1yH' 
 // import xpub
@@ -15,6 +26,12 @@ const eth_xpub = 'xpub6ERGFRzg9ptkXfZUmL9y7zRkvL58MBQyce61msQ9yBGFebD5Ha1LWsnfWK
 // import xpub
 var eth_xnode = eth_hdkey.fromExtendedKey(eth_xpub);
 
+
+function SvcErr (res, errStr) {
+    retStr = JSON.stringify({'error': errStr});
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(retStr);
+}
 
 function BtcSvcAddr (uid) {
     var wal = btc_xnode.derive(uid) // uid is a number
@@ -26,20 +43,6 @@ function EthSvcAddr (uid) {
     var wal = eth_xnode.deriveChild(uid) // userid is a number
     var addr = wal.getWallet().getChecksumAddressString();
     return JSON.stringify({'addr': addr});
-}
-
-function BtcSvc (q, res) {
-    var uid = parseInt(q.uid);
-    if (uid >= 0) {
-        var retStr = BtcSvcAddr(uid);
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(retStr);
-        return 
-    }
-
-    //error page
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.end("404 not found");
 }
 
 function btc_addr(res, req) {
@@ -54,9 +57,8 @@ function btc_addr(res, req) {
         return 
     }
 
-    //error page
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.end("404 not found");
+    //parameter error
+    SvcErr(res, 'parameter error');
 }
 
 function eth_addr(res, req) {
@@ -71,10 +73,95 @@ function eth_addr(res, req) {
         return 
     }
 
-    //error page
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.end("404 not found");
+    //parameter error
+    SvcErr(res, 'parameter error');
+}
+
+function btc_balance(res, req) {
+    console.log("Request handler 'btc_balance' was called.");
+
+    var q = url.parse(req.url, true).query;
+
+    if (bitcore.Address.isValid(q.addr)) {
+        insight.getUnspentUtxos(q.addr, function(error, utxo) {
+            if (error) {
+                //rpc error
+                SvcErr(res, err.toString());
+                return;
+            }
+
+            let balance = 0;
+            for (var i = 0; i < utxo.length; i++) {
+                balance +=utxo[i]['satoshis'];
+            }
+            balance = bitcore.Unit.fromSatoshis(balance).toBTC();
+            retStr = JSON.stringify({'balance': balance});
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(retStr);
+        });
+        
+        return 
+    }
+
+    //parameter error
+    SvcErr(res, 'parameter error');
+}
+
+function eth_balance(res, req) {
+    console.log("Request handler 'eth_balance' was called.");
+
+    var q = url.parse(req.url, true).query;
+    if (utils.isAddress(q.addr)) {
+        eth.getBalance(q.addr)
+        .then(ret => {
+            var balance = utils.fromWei(ret.toString(), 'ether');
+            retStr = JSON.stringify({'balance': balance});
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(retStr);
+        })
+        .catch(err => {
+            //rpc error
+            SvcErr(res, err.toString());
+        })
+
+        return 
+    }
+
+    //parameter error
+    SvcErr(res, 'parameter error');
+}
+
+const {cpcs_abi, cpcs_caddr} = require('./token/token.js')
+function cpcs_balance(res, req) {
+    console.log("Request handler 'cpcs_balance' was called.");
+
+    var q = url.parse(req.url, true).query;
+    if (utils.isAddress(q.addr)) {
+        var tcoin = new eth.Contract(cpcs_abi, cpcs_caddr); //token contract
+
+        //get balance
+        tcoin.methods.balanceOf(q.addr).call()
+        .then(ret => {
+            var balance = ret.toString();
+            retStr = JSON.stringify({'balance': balance});
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(retStr);
+        })
+        .catch(err => {
+            //rpc error
+            SvcErr(res, err.toString());
+        })
+
+        return 
+    }
+
+    //parameter error
+    SvcErr(res, 'parameter error');
 }
 
 exports.btc_addr = btc_addr;
 exports.eth_addr = eth_addr;
+exports.btc_balance = btc_balance;
+exports.eth_balance = eth_balance;
+exports.cpcs_balance = cpcs_balance;
+
